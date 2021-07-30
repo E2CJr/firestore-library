e02b8918fdf85804ba2061048ac70cb5db6d4c29
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-07-30 19:09:15 -0300

    crud base para clp

--- src/ClpProvider.js
+++ src/ClpProvider.js
@@ -1,4 +1,5 @@
 const { v4:uuidv4 } = require("uuid");
+const CompanyProvider = require("./CompanyProvider.js");
 const FirestoreConnection = require("./connection/FirestoreConnection");
 const { generateDocName } = require("./common/commons");
 
@@ -7,8 +8,103 @@ class ClpProvider extends FirestoreConnection {
   
   constructor(serviceAccount) {
     super(serviceAccount);
+    this.companyProvider = new CompanyProvider(serviceAccount);
   }
-  
+
+  async index(company) {
+		const hasCompany = await this.companyProvider.getById(company, true);
+
+		if (hasCompany.empty)
+			throw new Error("Empresa não encontrada");
+	
+		const clps = await this.db
+			.collection(this.collectionCompany)
+			.doc(hasCompany.docs[0].ref.path.split('/')[1])
+			.collection(this.collectionClp)
+			.get();
+		
+		return clps.docs.map(clp => clp.data());
+	}
+
+  async getById(company, id, ref=false) {
+    const hasCompany = await this.companyProvider.getById(company, true);
+
+		if (hasCompany.empty)
+			throw new Error("Empresa não encontrada");
+	
+		const clp = await this.db
+			.collection(this.collectionCompany)
+			.doc(hasCompany.docs[0].ref.path.split('/')[1])
+			.collection(this.collectionClp)
+      .where("id", "==", id)
+      .get();
+
+		if (ref) return clp;
+
+		if (clp.empty) return null;
+				
+		return clp.docs[0].data();
+  }
+
+  async save(company, data) {
+		const hasCompany = await this.companyProvider.getById(company, true);
+		
+		if (hasCompany.empty) 
+			throw new Error("Empresa não cadastrada");
+				
+		const document = this.db
+			.collection(this.collectionCompany)
+			.doc(hasCompany.docs[0].ref.path.split('/')[1])
+			.collection(this.collectionClp)
+			.doc(generateDocName());
+
+		return await document.set({ 
+			...data,
+			id: uuidv4(),
+		});
+	}
+
+  async update(company, id, data) {
+		const hasCompany = await this.companyProvider.getById(company, true);
+		
+		if (hasCompany.empty) 
+			throw new Error("Empresa não cadastrada");
+		
+		const document = await this.db
+			.collection(this.collectionCompany)
+			.doc(hasCompany.docs[0].ref.path.split('/')[1])
+			.collection(this.collectionClp)
+			.where("id", "==", id)
+			.get();
+
+		if (document.empty) 
+			throw new Error("ID não encontrado");
+
+		await document.docs[0].ref.update({
+			...data,
+			id: document.docs[0].data().id,
+		});
+	}
+
+	async delete(company, id) {
+		const hasCompany = await this.companyProvider.getById(company, true);
+		
+		if (hasCompany.empty) 
+			throw new Error("Empresa não cadastrada");
+		
+		const document = await this.db
+			.collection(this.collectionCompany)
+			.doc(hasCompany.docs[0].ref.path.split('/')[1])
+			.collection(this.collectionClp)
+			.where("id", "==", id)
+			.get();
+
+		if (document.empty) 
+			throw new Error("ID não encontrado");
+
+		await document.docs[0].ref.delete();
+	}
+
 }
 
 module.exports = ClpProvider;
