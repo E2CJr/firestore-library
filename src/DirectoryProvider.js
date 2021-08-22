432db05c73207787a2ae09128bd95eb902aeb2ef
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-08-22 13:11:05 -0300

    lista e recuperacao por id de diretorio

--- src/DirectoryProvider.js
+++ src/DirectoryProvider.js
@@ -1,5 +1,4 @@
-const { v4:uuidv4 } = require("uuid");
-const { generateDocName } = require("./common/commons");
+const { generateDocName, getPath } = require("./common/commons");
 const CompanyProvider = require("./CompanyProvider.js");
 const FirestoreConnection = require("./connection/FirestoreConnection");
 
@@ -9,6 +8,39 @@ class DirectoryProvider extends FirestoreConnection {
   constructor(serviceAccount) {
     super(serviceAccount);
     this.companyProvider = new CompanyProvider(serviceAccount);
+  }
+  
+  async index(company) {
+		const hasCompany = await this.companyProvider.getById(company, true);
+
+		if (hasCompany.empty)
+			throw new Error("Empresa não encontrada");
+	
+		const directories = await this.db
+			.collection(this.collectionCompany)
+			.doc(hasCompany.docs[0].ref.path.split('/')[1])
+			.collection(this.collectionDirectory)
+      .get();
+				
+		return directories.docs.map(doc => doc.data());
+	}
+
+	async getById(company, id, ref=false) {
+    const hasCompany = await this.companyProvider.getById(company, true);
+
+		if (hasCompany.empty)
+			throw new Error("Empresa não encontrada");
+	
+		const folder = await this.db
+			.collection(this.collectionCompany)
+			.doc(hasCompany.docs[0].ref.path.split('/')[1])
+			.collection(this.collectionDirectory)
+      .where("id", "==", id)
+      .get();
+
+		if (ref) return folder;
+				
+		return folder.empty ? null : folder.docs[0].data();
   }
   
 }
